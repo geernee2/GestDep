979b9fe1b7ba317271bd5c868b854c049ac6eedc
geernee2 <74990122+geernee2@users.noreply.github.com>
2021-12-08 19:23:19 +0000

    Add files via upload

---
+++ HellowWorld/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HellowWorld
+{
+    class Program
+    {
+        private static List<string> nacionalities;
+        private static void InitList()
+        {
+            nacionalities = new List<string>()
+             {
+
+             "Italiano",      
+             "Australian",
+             "Mongolian",
+             "Russian",
+             "Austrian",
+             "Brazilian"
+             };
+        }
+        static void Main(string[] args)
+        {
+            InitList();
+            nacionalities.Sort();
+            foreach (string nacionality in nacionalities)
+            {
+                Console.WriteLine(nacionality);
+            }
+        }
+    }
+}
